30dba8d916d50bb6b7b7c849af80534711dd4fb9
Álvaro de Marcos Alés <almaral4c@gmail.com>
2017-04-06 12:32:10 +0200

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+using System.Text;
+
+public class Inventory : MonoBehaviour{
+
+	private List<Allie> dataBase = new List<Allie> ();
+	private JsonData itemData;
+	public List<Item> myItems = new List<Item> ();
+
+
+	void Start (){
+		itemData = JsonMapper.ToObject (File.ReadAllText (Application.dataPath + "/Inventory/Items.json"));
+		ReadDataBase ();
+	}
+
+	private void ReadDataBase (){
+
+		for (int i = 0; i < itemData.Count; i++){
+	
+			dataBase.Add (new Allie ((int)itemData [i] ["Movement"], (int)itemData [i] ["AttackDistance"], (int)itemData [i] ["Level"], 
+				(int)itemData [i] ["Experience"], (int)itemData [i] ["Stats"] ["Hp"], (int)itemData [i] ["Stats"] ["Strength"],
+				(int)itemData [i] ["Stats"] ["Skill"], (int)itemData [i] ["Stats"] ["Speed"], (int)itemData [i] ["Stats"] ["Luck"],
+				(int)itemData [i] ["Stats"] ["Defence"], (int)itemData [i] ["Stats"] ["Resistance"], (string)itemData [i] ["Mount"],
+				(int)itemData [i] ["Complexity"], (int)itemData [i] ["Aid"]));
+	
+			for (int j = 0; j < itemData [i] ["Items"].Count; j++){
+
+				if (itemData [i] ["Items"] [j].Count > 0){
+					myItems.Add (new Item ((int)itemData [i] ["Items"] [j] ["ProcedenceID"], (string)itemData [i] ["Items"] [j] ["Image"], (string)itemData [i] ["Items"] [j] ["Type"], (string)itemData [i] ["Items"] [j] ["Name"],
+						(string)itemData [i] ["Items"] [j] ["Description"], (int)itemData [i] ["Items"] [j] ["Quantity"], (int)itemData [i] ["Items"] [j] ["Attack"], (int)itemData [i] ["Items"] [j] ["Critical"],
+						(int)itemData [i] ["Items"] [j] ["Accuracy"], (int)itemData [i] ["Items"] [j] ["Dodge"]));
+				}
+			}
+		}
+	}
+
+	public void RemoveItem (int AllieID, int ID){
+
+		int numItemsBefore = 0;
+
+		for (int i = 0; i < AllieID; i++){
+			numItemsBefore += getAllieItems (i).Count;
+		}
+
+		myItems.RemoveAt (ID + numItemsBefore);
+
+
+
+		List<Item> allieItems = getAllieItems (AllieID);
+		for (int j = 0; j < allieItems.Count; j++){
+			itemData [AllieID] ["Items"] [j] ["ProcedenceID"] = allieItems [j].ProcedenceID;
+			itemData [AllieID] ["Items"] [j] ["Image"] = allieItems [j].Image;
+			itemData [AllieID] ["Items"] [j] ["Type"] = allieItems [j].Type;
+			itemData [AllieID] ["Items"] [j] ["Name"] = allieItems [j].Name;
+			itemData [AllieID] ["Items"] [j] ["Description"] = allieItems [j].Description;
+			itemData [AllieID] ["Items"] [j] ["Quantity"] = allieItems [j].Quantity;
+			itemData [AllieID] ["Items"] [j] ["Attack"] = allieItems [j].Attack;
+			itemData [AllieID] ["Items"] [j] ["Critical"] = allieItems [j].Critical;
+			itemData [AllieID] ["Items"] [j] ["Accuracy"] = allieItems [j].Accuracy;
+			itemData [AllieID] ["Items"] [j] ["Dodge"] = allieItems [j].Dodge;
+		}
+
+		
+		itemData [AllieID] ["Items"] [allieItems.Count].Clear ();
+
+
+		itemData = JsonMapper.ToJson (itemData); 
+		File.WriteAllText (Application.dataPath + "/Inventory/Items.json", itemData.ToString ());
+		dataBase.Clear ();
+		myItems.Clear ();
+		itemData = JsonMapper.ToObject (File.ReadAllText (Application.dataPath + "/Inventory/Items.json"));
+		ReadDataBase ();
+	}
+
+	public void SwapItems (int AllieID, int ID1, int ID2){
+
+		string pathImage = (string)itemData [AllieID] ["Items"] [ID1] ["Image"];
+		Item aux = new Item ((int)itemData [AllieID] ["Items"] [ID1] ["ProcedenceID"], (string)itemData [AllieID] ["Items"] [ID1] ["Image"], (string)itemData [AllieID] ["Items"] [ID1] ["Type"], (string)itemData [AllieID] ["Items"] [ID1] ["Name"],
+			           (string)itemData [AllieID] ["Items"] [ID1] ["Description"], (int)itemData [AllieID] ["Items"] [ID1] ["Quantity"], (int)itemData [AllieID] ["Items"] [ID1] ["Attack"], (int)itemData [AllieID] ["Items"] [ID1] ["Critical"],
+			           (int)itemData [AllieID] ["Items"] [ID1] ["Accuracy"], (int)itemData [AllieID] ["Items"] [ID1] ["Dodge"]);
+		
+
+		itemData [AllieID] ["Items"] [ID1] ["ProcedenceID"] = (int)itemData [AllieID] ["Items"] [ID2] ["ProcedenceID"];
+		itemData [AllieID] ["Items"] [ID1] ["Image"] = (string)itemData [AllieID] ["Items"] [ID2] ["Image"];
+		itemData [AllieID] ["Items"] [ID1] ["Type"] = (string)itemData [AllieID] ["Items"] [ID2] ["Type"];
+		itemData [AllieID] ["Items"] [ID1] ["Name"] = (string)itemData [AllieID] ["Items"] [ID2] ["Name"];
+		itemData [AllieID] ["Items"] [ID1] ["Description"] = (string)itemData [AllieID] ["Items"] [ID2] ["Description"];
+		itemData [AllieID] ["Items"] [ID1] ["Quantity"] = (int)itemData [AllieID] ["Items"] [ID2] ["Quantity"];
+		itemData [AllieID] ["Items"] [ID1] ["Attack"] = (int)itemData [AllieID] ["Items"] [ID2] ["Attack"];
+		itemData [AllieID] ["Items"] [ID1] ["Critical"] = (int)itemData [AllieID] ["Items"] [ID2] ["Critical"];
+		itemData [AllieID] ["Items"] [ID1] ["Accuracy"] = (int)itemData [AllieID] ["Items"] [ID2] ["Accuracy"];
+		itemData [AllieID] ["Items"] [ID1] ["Dodge"] = (int)itemData [AllieID] ["Items"] [ID2] ["Dodge"];
+
+
+		itemData [AllieID] ["Items"] [ID2] ["ProcedenceID"] = aux.ProcedenceID;
+		itemData [AllieID] ["Items"] [ID2] ["Image"] = pathImage;
+		itemData [AllieID] ["Items"] [ID2] ["Type"] = aux.Type;
+		itemData [AllieID] ["Items"] [ID2] ["Name"] = aux.Name;
+		itemData [AllieID] ["Items"] [ID2] ["Description"] = aux.Description;
+		itemData [AllieID] ["Items"] [ID2] ["Quantity"] = aux.Quantity;
+		itemData [AllieID] ["Items"] [ID2] ["Attack"] = aux.Attack;
+		itemData [AllieID] ["Items"] [ID2] ["Critical"] = aux.Critical;
+		itemData [AllieID] ["Items"] [ID2] ["Accuracy"] = aux.Accuracy;
+		itemData [AllieID] ["Items"] [ID2] ["Dodge"] = aux.Dodge;
+
+		itemData = JsonMapper.ToJson (itemData); 
+		File.WriteAllText (Application.dataPath + "/Inventory/Items.json", itemData.ToString ());
+		dataBase.Clear ();
+		myItems.Clear ();
+		itemData = JsonMapper.ToObject (File.ReadAllText (Application.dataPath + "/Inventory/Items.json"));
+		ReadDataBase ();
+	}
+
+	public void ChangeValues (int ID, string Field, int Value){
+
+		itemData [ID] [Field] = Value; // change
+
+		itemData = JsonMapper.ToJson (itemData);
+		File.WriteAllText (Application.dataPath + "/Inventory/Items.json", itemData.ToString ());
+		dataBase.Clear ();
+		myItems.Clear ();
+		itemData = JsonMapper.ToObject (File.ReadAllText (Application.dataPath + "/Inventory/Items.json"));
+		ReadDataBase ();
+
+	}
+
+	public void ChangeValuesObject (int ID, int Value, int ID2, string Field2){
+
+		itemData [ID] ["Items"] [ID2] [Field2] = Value; // change
+
+		itemData = JsonMapper.ToJson (itemData); 
+
+		File.WriteAllText (Application.dataPath + "/Inventory/Items.json", itemData.ToString ());
+		dataBase.Clear ();
+		myItems.Clear ();
+		itemData = JsonMapper.ToObject (File.ReadAllText (Application.dataPath + "/Inventory/Items.json"));
+		ReadDataBase ();
+
+	}
+
+
+	public Allie getAllie (int i){ // el Allie.ID concuerda con la posicion en la Array del .json
+		return dataBase [i];
+	}
+
+	public List<Item> getAllieItems (int id){ // devuelve una lista de todos los items de un alliado
+		List<Item> sol = new List<Item> ();
+
+		for (int i = 0; i < myItems.Count; i++){
+			if (myItems [i].ProcedenceID == id){
+				sol.Add (myItems [i]);
+			} else if (myItems [i].ProcedenceID > id){
+				break;
+			}	
+		}
+		return sol;
+	}
+
+}
---
+++ Map.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+	
+	public Spot [][] matrixScenary;
+
+	public int Right;
+	public int Up;
+
+
+	void Start () {
+
+		matrixScenary = new Spot[Right+1][];
+
+		for(int i = 0; i <= Right; i++){
+			matrixScenary[i] = new Spot[Up+1];
+			for(int j = 0; j <= Up; j++){
+				matrixScenary[i][j] = gameObject.AddComponent<Spot>(); // == new Spot();
+				matrixScenary[i][j].SetPoint(i,j); // dudoso
+				matrixScenary[i][j].setType(Types.Free);
+				matrixScenary[i][j].setPersonType(null);
+			}
+		}
+
+		for(int i = 0; i <= Right; i++){	// mejorar eficiencia
+			for(int j = 0; j <= Up; j++){
+				matrixScenary[i][j].addNeighbors(matrixScenary,Right,Up);
+			}
+		}
+
+	}
+
+	public string GetValue(int a,int b){
+		return matrixScenary[a][b].personType;
+	}
+
+	public void SetValue(int a, int b, string c){
+		matrixScenary[a][b].personType = c;
+	}
+
+
+	public string GetValueScenary(int a,int b){
+		return matrixScenary[a][b].type;
+	}
+
+	public void EndTurn(){
+		Object[] objs = Allie.FindSceneObjectsOfType(typeof(Allie));
+		foreach (Allie a in objs){
+			a.setDisponibility(true);
+		}
+	}
+
+
+	private void RemoveFromArray (Spot[] myArray, Spot elem,int pos){ // quita de open set el spot prometedor		*****Plantear hacer quicksort y despues binary search
+
+		for(int i = pos-1; i >= 0; i--){ // por como esta definida deberia hacerlo de princi
+			if(myArray[i].equal(elem)){
+				for(int j = i; j< myArray.Length-1; j++){
+					myArray[j] = myArray[j+1];
+				}
+				break;			
+			}
+		} 
+	}
+
+	private bool isIncluded (Spot a, Spot[] list,int pos){
+
+		for(int i = 0; i < pos; i++){
+			if(list[i].equal(a)){
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private int heuristic (Spot a, Spot b){
+		return Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y -b.y);
+	}
+
+	private int distance (Allie myAllies,Spot start){
+
+		int myAllieX = Mathf.FloorToInt(myAllies.transform.position.x);
+		int myAllieY = Mathf.FloorToInt(myAllies.transform.position.y);
+
+		return Mathf.Abs((myAllieX-start.x)+ (myAllieY - start.y));
+	}
+
+	private int distBetween (Spot a,Spot b){
+		// cambiar free de cursor a ocuppied con sus implicaciones
+		if(b.type == Types.Forest){
+		return 2;
+		}else{
+		return 1;
+		}
+	}
+
+
+	public bool isPathFinding(int startX , int startY, int endX, int endY,Allie myAllie,Spot [] path,Spot posInPath){
+		
+		int posInOpen = 0;
+		int posInClose = 0;
+
+
+		Spot start = gameObject.AddComponent<Spot>(); // start and end generated
+		Spot end = gameObject.AddComponent<Spot>();
+		start.SetPoint(startX,startY);
+		end.SetPoint(endX,endY);
+		start.setType(Types.Free);
+		end.setType(Types.Free);
+
+		start.addNeighbors(matrixScenary,Right,Up);
+		end.addNeighbors(matrixScenary,Right,Up);
+
+		Spot [] openSet;	//open and close set generated and initiallized
+		Spot [] closeSet;
+		openSet = new Spot[(Right+1)*(Up+1)];
+		closeSet = new Spot[(Right+1)*(Up+1)];
+		openSet[posInOpen]  = start;
+		posInOpen++;
+		bool ok = false;
+
+		while (posInOpen > 0 && !ok){
+
+			int prometedor = 0; // buscamos de la lista de los posibles neighbors cual es el que tiene una f mas baja
+			for(int i = 0; i < posInOpen; i++){ 
+				if(openSet[i].getF() < openSet[prometedor].getF() && distance(myAllie,openSet[i]) < myAllie.movement){ // la distancia entre inicio y prometedor es menor que myallie.movement
+					prometedor = i;
+
+				}
+			}
+
+			Spot current = openSet[prometedor];
+			if(current.equal(end)){ // si el prometedor es el final
+
+				 // intentar reducirlo despues
+				
+
+				Spot temp = current;
+				path[posInPath.x] = temp;
+				posInPath.x++;
+
+				while(temp.previous){
+					path[posInPath.x] = temp.previous;
+					posInPath.x++;
+					temp = temp.previous;
+
+				}
+
+				ok = true;
+				if(path[0].g > myAllie.movement){ // se deberia comparar la g con el movement de myAllie
+					Destroy(start);
+					Destroy(end);
+					return false;
+				}
+				Destroy(start);
+				Destroy(end);
+				return ok;
+			}
+
+			RemoveFromArray(openSet,current,posInOpen);
+			System.Array.Resize<Spot>(ref openSet,openSet.Length-1);
+			posInOpen--;
+			closeSet[posInClose] = current; // añadimos a los visitados el nodo prometedor
+			posInClose++;
+
+
+			Spot [] neighborsOfPrometedor = current.getNeighbors();
+
+			for(int i = 0; i < current.numNeigh; i++){
+				
+				if(!isIncluded(neighborsOfPrometedor[i], closeSet,posInClose) && neighborsOfPrometedor[i].type != Types.Ocupaid){ 
+					
+					int TempG = current.g + distBetween(current,neighborsOfPrometedor[i]); // lo que cuesta llegar al neighbor
+					bool newPath = false;
+					if(isIncluded(neighborsOfPrometedor[i], openSet,posInOpen)){
+						if(TempG < neighborsOfPrometedor[i].g){
+							neighborsOfPrometedor[i].g = TempG;
+							newPath = true;
+						}
+					}else{
+						neighborsOfPrometedor[i].g = TempG;
+						newPath = true;
+						openSet[posInOpen] = neighborsOfPrometedor[i];
+						posInOpen++;
+					}
+					if(newPath){
+						neighborsOfPrometedor[i].h = heuristic(neighborsOfPrometedor[i],end);		// neighborsOfPrometedor[i] = neighbor
+						neighborsOfPrometedor[i].f = neighborsOfPrometedor[i].g +neighborsOfPrometedor[i].h;
+						neighborsOfPrometedor[i].previous = current;
+					}
+				}
+			}
+
+
+		}
+
+		Destroy(start);
+		Destroy(end);
+		return ok;
+
+		// cuando saliese seria no solucion
+
+	}
+
+
+
+}
